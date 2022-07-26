3c33dc9e02532e9dff4e33220b86f61d07d88081
Hans Kinkelaar <hanskinkelaar92@gmail.com>
2022-07-26 16:48:30 -0300

    arrumando cagada

--- front-vue-v2/src/main.js
+++ front-vue-v2/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(VueSweetalert2);
 
 new Vue({
   router,
-  render: (h) => h(App),
+  render: (h) => h(App), 
 }).$mount('#app');
--- front-vue-v2/src/views/HomeView.vue
+++ front-vue-v2/src/views/HomeView.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1 class="jumbotron">Teste Aplicação Vue + Bootstrap</h1>
 </template>
 
 <script>
