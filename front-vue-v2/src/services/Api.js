ccd0defe3530885c0d1ec990211742249cf88eeb
Hans Kinkelaar <hanskinkelaar92@gmail.com>
2022-08-01 17:56:18 -0300

    Fix no Api.js pro ESLINT não reclamar.

--- front-vue-v2/src/services/Api.js
+++ front-vue-v2/src/services/Api.js
@@ -4,9 +4,9 @@
  *  description: file what inicialize 'axios' and the requests from HTTP's
  */
 
-import axios from "axios";
+import axios from 'axios';
 
 export default () => axios.create({
-    // 'baseURL' do Back-End -> fará a comunicação do Front com o Back
-    baseURL: 'http://localhost:3000/api'
-})
+  // 'baseURL' do Back-End -> fará a comunicação do Front com o Back
+  baseURL: 'http://localhost:3000/api',
+});
